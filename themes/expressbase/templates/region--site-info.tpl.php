214af88e13d266c226069f64b0bcb0f640934876
stalkert <Tim.Stalker@ucdenver.edu>
2017-09-29 11:00:25 -0600

    removing title from image per tenon.io accessibility check

--- themes/expressbase/templates/region--site-info.tpl.php
+++ themes/expressbase/templates/region--site-info.tpl.php
@@ -20,7 +20,7 @@
 <div id="Contentplaceholder3_T9F43D663012_Col00" class="sf_colsIn container" data-sf-element="Container" data-placeholder-label="Container">
 <div class="t-contentBlock">
 
-    <div><div id="cuicContainer"><img alt="CU in the City logo" id="cuic" src="<?php print $base_url . '/' . drupal_get_path('theme', 'expressbase'); ?>/images/cudenver/cuic4.png" data-displaymode="Original" title="CU in the City"></div></div>    
+    <div><div id="cuicContainer"><img alt="CU in the City logo" id="cuic" src="<?php print $base_url . '/' . drupal_get_path('theme', 'expressbase'); ?>/images/cudenver/cuic4.png" data-displaymode="Original"></div></div>    
 
 </div>
 
