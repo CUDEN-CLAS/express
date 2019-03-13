53cd1bb6193b7d22d85f80914acdcc8610a6197f
Tim Stalker <Tim.Stalker@ucdenver.edu>
2019-03-13 15:34:14 -0600

    adding CU Denver theme

---
+++ themes/ucd/templates/region--site-info.tpl.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for the footer region.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728140
+ */
+?>
+
+  <footer id="site-footer" class="row <?php print $classes; ?>">
+    <?php if ($content): ?>
+      <div id="site-footer-content" class="col-lg-12 col-md-12">
+        <?php print $content; ?>
+      </div>
+    <?php endif; ?>
+    
+    <div class="clearfix"></div>
+  </footer>
+<div id="Contentplaceholder3_T9F43D663012_Col00" class="sf_colsIn container" data-sf-element="Container" data-placeholder-label="Container">
+<div class="t-contentBlock">
+
+    <div><div id="cuicContainer"><img alt="CU in the City logo" id="cuic" src="<?php print $base_url . '/' . drupal_get_path('theme', 'ucd'); ?>/images/cudenver/cuic4.png" data-displaymode="Original"></div></div>    
+
+</div>
+
+</div>
